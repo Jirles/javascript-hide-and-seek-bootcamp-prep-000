0d836dfa41dfa106b2149e0c3fdf1d0ef7b73e20
Emily Jirles <emily.jirles@gmail.com>
2018-02-21 13:59:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ function increaseRankBy(n){
 }
 
 function deepestChild(){
-  var grandNodeChildren = document.querySelectorAll('#grand-node'>)
+  var grandNodeChildren = document.querySelectorAll('#grand-node'>*)
   return 'hold your horses'
 }
